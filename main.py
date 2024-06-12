b75e5df27eca1f3ef842efa0a5526fb38c66b63d
HaroldC8 <70860865+HaroldC8@users.noreply.github.com>
2024-06-12 21:05:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ Width = 2480
 Height = 3508
 Border = 100
 Spacing = 4
+IconWidth = 556
+IconHeight = 698
 
 # Calculate next card position based on iteration
-def layer_pos(sizeX, sizeY, iteration):
-	mod = math.floor((Width - 2*Border) / (sizeX + Spacing))
-	x = (int)(Border + (Spacing + sizeX) * (iteration % mod))
-	y = (int)(Border + (Spacing + sizeY) * math.floor(iteration/mod))
+def layer_pos(sizeX, sizeY, width, border, spacing, iteration):
+	mod = math.floor((width - (2 * border)) / (sizeX + spacing))
+	x = (int)(border + (spacing + sizeX) * (iteration % mod))
+	y = (int)(border + (spacing + sizeY) * math.floor(iteration / mod))
 	return x, y
 
 def save_image(outImage, infile, j):
     outfile = os.path.join(os.path.dirname(infile), "processed{}.png".format(j))
-    print "Saving to %s" % outfile
     pdb.gimp_image_merge_visible_layers(outImage, 1)
     pdb.gimp_palette_set_background('white')
     pdb.gimp_image_flatten(outImage)
@@ -23,7 +24,7 @@ def save_image(outImage, infile, j):
     print "Saved to %s" % outfile
     return gimp.Image(Width, Height, RGB)
 
-def process(infile, outImage, values, iteration):
+def process(infile, outImage, values, icons, iteration, realIteration):
     print "Processing file %s " % infile
     inImage = pdb.gimp_file_load(infile, infile, run_mode=RUN_NONINTERACTIVE)
     i = 0
@@ -43,13 +44,22 @@ def process(infile, outImage, values, iteration):
 		if "ChangePosImage" in layer.name:
 			print "Changing image layer %s y pos to %s" % (layer.name, 90*(9-int(values[2])))
 			layer.set_offsets(562, 90*(9-int(values[2])))
+	# Add icon image
+    iconX, iconY = layer_pos(IconWidth, IconHeight, icons.active_layer.width, 0, 0, realIteration)
+    pdb.gimp_image_select_rectangle(icons, CHANNEL_OP_REPLACE, iconX, iconY, IconWidth, IconHeight)
+    pdb.gimp_edit_copy(icons.active_layer)
+    pastedIcon = gimp.Layer(inImage, "icon", inImage.active_layer.width, inImage.active_layer.height, RGBA_IMAGE, 100, NORMAL_MODE)
+    inImage.add_layer(pastedIcon, len(inImage.layers)-3)
+    pastedIcon = pdb.gimp_edit_paste(pastedIcon, FALSE)
+    pastedIcon.set_offsets(100, 100)
+    # Add card to output image
     single_layer = pdb.gimp_image_merge_visible_layers(inImage, 1)
-    copy_layer = gimp.Layer(outImage, "copytest", Width, Height, RGBA_IMAGE, 100, NORMAL_MODE)
+    copy_layer = gimp.Layer(outImage, "card", Width, Height, RGBA_IMAGE, 100, NORMAL_MODE)
     outImage.add_layer(copy_layer, 1)
     pdb.gimp_edit_copy(single_layer)
     copy_layer = pdb.gimp_edit_paste(copy_layer, FALSE)
-    offsetX, offsetY = layer_pos(copy_layer.width, copy_layer.height, iteration)
-    print "Success1 %s %s" % (offsetX, offsetY)
+    offsetX, offsetY = layer_pos(copy_layer.width, copy_layer.height, Width, Border, Spacing, iteration)
+    print "Adding card to output image"
     copy_layer.set_offsets(offsetX, offsetY)
 
 def run(directory):
@@ -57,8 +67,9 @@ def run(directory):
     print "Running on directory \"%s\"" % directory
     textfile = glob.glob(os.path.join(directory, '*.txt'))
     infiles = glob.glob(os.path.join(directory, '*.xcf'))
-    if len(infiles) == 0 or len(textfile) == 0:
-	    print "ERORR: no .xcf or .txt input files in provided directory!"
+    iconFile = os.path.join(directory, 'image.png')
+    if len(infiles) == 0 or len(textfile) == 0 or len(iconFile) == 0:
+	    print "ERORR: missing some input files in provided directory!"
 	    return
     text = open(textfile[0], "r")
     lines = text.readlines()
@@ -66,15 +77,16 @@ def run(directory):
 	    print "ERORR: .txt file is in an incompatible format!"
 	    return
     inImage = pdb.gimp_file_load(infiles[0], infiles[0], run_mode=RUN_NONINTERACTIVE)
+    icons = pdb.file_png_load(iconFile, iconFile)
     outImage = gimp.Image(Width, Height, RGB)
     j = 0
     it = 0
     for i in range(0, len(lines)/4):
-	    if layer_pos(inImage.width, inImage.height, it)[1] + inImage.height + Border > Height:
+	    if layer_pos(inImage.width, inImage.height, Width, Border, Spacing, it)[1] + inImage.height + Border > Height:
 		    outImage = save_image(outImage, infiles[0], j)
 		    j += 1
 		    it = 0
-	    process(infiles[0], outImage, lines[4*i:4*i+4], it)
+	    process(infiles[0], outImage, lines[4*i:4*i+4], icons, it, i)
 	    it += 1
     text.close()
     save_image(outImage, infiles[0], j)
@@ -83,3 +95,4 @@ def run(directory):
 
 if __name__ == "__main__":
     print "Running as __main__ with args: %s" % sys.argv
+    run("C:\Users\User\Documents\Art\Cards\Exports")
